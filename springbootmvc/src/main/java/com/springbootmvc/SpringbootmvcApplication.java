8a1dc81e36cf8c8a27e1ad25641c254ec9c0427e
Nazia <oneplus.nf@gmail.com>
2020-11-07 21:55:33 +0530

    Clean up

--- springbootmvc/src/main/java/com/springbootmvc/SpringbootmvcApplication.java
+++ springbootmvc/src/main/java/com/springbootmvc/SpringbootmvcApplication.java
@@ -2,16 +2,10 @@ package com.springbootmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class SpringbootmvcApplication extends SpringBootServletInitializer {
-	
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SpringbootmvcApplication.class);
-    }
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootmvcApplication.class, args);
